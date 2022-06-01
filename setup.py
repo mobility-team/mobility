234a811cad622b33ec9c2b0ffab5629ab5fa7c4b
Félix Pouchain <flx.pouchain@gmail.com>
2022-06-01 17:26:00 +0200

    add setuptools and requirements file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="mobilite",
+    version="0.0.1",
+    author="Antoine Gauchot, Anne-Sophie Girot, Louise Gontier, Félix Pouchain",
+    author_email="antoine.gauchot@arep.fr, a.girot@elioth.fr, l.gontier@elioth.fr, felix.pouchain@arep.fr",
+    description="A tool to simulate the mobility behaviours of the inhabitants of a given region.",
+    url="https://github.com/mobility-team/mobility",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9',
+    install_requires=[
+        "numpy",
+        "pandas"
+    ]
+)
