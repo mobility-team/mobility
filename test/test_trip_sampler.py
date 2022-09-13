38b31d9e3605a37c934c1f400eefe9e37354c42e
GONTIER Louise <l.gontier@elioth.fr>
2022-09-13 16:00:08 +0200

    Add carbon computation to example file

--- test/test_trip_sampler.py
+++ test/test_trip_sampler.py
@@ -1,12 +1,18 @@
 import mobility
+from mobility.carbon_computation import carbon_computation
 
-def test_trip_sampler():
-	ts = mobility.TripSampler()
-	trips = ts.get_trips(
-		csp="3",
-		csp_household="3",
-		urban_unit_category="C",
-		n_pers="2",
-		n_cars="0",
-		n_years=1
-	)
+# Create trip sampler
+ts = mobility.TripSampler()
+
+# Get annual trips of an individual of csp=3, living in a city center with another person  
+trips = ts.get_trips(
+  csp="3",
+  csp_household="3",
+  urban_unit_category="C",
+  n_pers="2",
+  n_cars="0",
+  n_years=1
+)
+
+# Compute carbon emissions for each trip
+emissions = carbon_computation(trips)
