5201fb4d9396694bc5b97d7c2d693f15386de3e3
Cap <capucine-marin.dubroca-voisin@arep.fr>
2023-04-21 10:20:32 +0200

    Create conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,20 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'Mobility'
+copyright = '2023, MIT Licence'
+author = 'Mutliple authors'
+
+release = '0.1'
+version = '0.1'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
