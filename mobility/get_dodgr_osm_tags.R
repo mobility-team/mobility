bd036a4b699c85149c8a7ebe23a63be6b124b9c4
Felix Pouchain <flx.pouchain@gmail.com>
2024-01-03 15:14:08 +0100

    create RScript python class + create separate R packages installations scripts called from mobility.setup + distribute a forked version of the osmdata R package as a binary to speed up OSM data parsing + switch to pyproject.toml

--- mobility/get_dodgr_osm_tags.R
+++ mobility/get_dodgr_osm_tags.R
@@ -1,16 +1,9 @@
-source("mobility/load_packages.R")
-packages <- c("dodgr", "optparse")
-load_packages(packages)
+library(dodgr)
 
-option_list = list(
-  make_option(c("-d", "--dodgr-mode"), type = "character")
-)
-
-opt_parser = OptionParser(option_list = option_list)
-opt = parse_args(opt_parser)
+dodgr_mode <- commandArgs(trailingOnly = TRUE)
 
 profiles <- dodgr::weighting_profiles$weighting_profiles
-highway_tags <- profiles[profiles$name == opt[["dodgr-mode"]] & !is.na(profiles$max_speed) & profiles$value > 0.0, "way"]
+highway_tags <- profiles[profiles$name == dodgr_mode & !is.na(profiles$max_speed) & profiles$value > 0.0, "way"]
 highway_tags <- paste(highway_tags, collapse = ",")
 
 cat(paste(highway_tags, collapse = ","))
