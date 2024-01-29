ebed714206d851630afd8c4ea7e8f8dc94d70d19
Felix Pouchain <flx.pouchain@gmail.com>
2024-01-29 17:18:31 +0100

    create two distinct classes TravelCosts (car, walk, bicycle) and PublicTravelCosts (all public transport modes) + create a GTFS class to handle download / aggregation / GTFS router object preparation

--- mobility/__init__.py
+++ mobility/__init__.py
@@ -2,4 +2,4 @@ from .set_params import set_params
 from .trip_sampler import TripSampler
 from .transport_zones import TransportZones
 from .travel_costs import TravelCosts
-from .gtfs import GTFS
+from .public_transport_travel_costs import PublicTransportTravelCosts
