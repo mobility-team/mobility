aaa60c9c1365c311111e4fd63fb1ec9a6f0e6bce
Cap <capucine-marin.dubroca-voisin@arep.fr>
2024-02-13 23:13:46 +0100

    Update test_transport_zones.py

--- tests/test_transport_zones.py
+++ tests/test_transport_zones.py
@@ -2,6 +2,8 @@ import os
 import dotenv
 import mobility
 
+mobility.set_params()
+
 def test_pub_trans():
   dotenv.load_dotenv()
   transport_zones = mobility.TransportZones("31404", method="radius", radius=40)
