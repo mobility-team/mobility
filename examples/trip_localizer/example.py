597a3b42a673e8edecdfe2517807b0988182188a
Felix Pouchain <flx.pouchain@gmail.com>
2024-02-21 16:14:11 +0100

    fix cantville code for marseille in census data + break the localize_trips method into small submethods

--- examples/trip_localizer/example.py
+++ examples/trip_localizer/example.py
@@ -13,7 +13,7 @@ mobility.set_params(
 # Generate localized and non-localized trips for individuals sampled 
 # from the population of each transport zone around Lyon 
 # (takes ~ 30 min for now, reduce the radius and the sample size if you want faster results)
-transport_zones = mobility.TransportZones("69383", method="radius", radius=30.0)
+transport_zones = mobility.TransportZones("13201", method="radius", radius=30.0)
 population = mobility.Population(transport_zones, sample_size=10000)
 trips = mobility.Trips(population)
 loc_trips = mobility.LocalizedTrips(trips)
