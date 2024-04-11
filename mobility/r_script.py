fb52d0eaa632a2d289f92ad2817223725dbef2d5
Felix Pouchain <flx.pouchain@gmail.com>
2024-04-11 16:39:10 +0200

    test all

--- mobility/r_script.py
+++ mobility/r_script.py
@@ -19,13 +19,9 @@ class RScript:
     def run(self, args: list) -> None:
         cmd = ["Rscript", self.script_path] + args
 
-        logging.info("creating subprocess")
-
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
         output_thread = threading.Thread(target=self.print_output, args=(process.stdout,))
 
-        logging.info("creating thread")
         output_thread.start()
         process.wait()
         output_thread.join()
@@ -33,8 +29,7 @@ class RScript:
     def print_output(self, stream):
         for line in iter(stream.readline, b""):
             msg = line.decode("utf-8", errors="replace")
-            print(msg)
-            # if "INFO" in msg:
-            #     msg = msg.split("]")[1]
-            #     msg = msg.strip()
-            #     logging.info(msg)
+            if "INFO" in msg:
+                msg = msg.split("]")[1]
+                msg = msg.strip()
+                logging.info(msg)
