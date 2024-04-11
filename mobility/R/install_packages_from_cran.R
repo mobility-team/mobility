807d06371a7bfd81b53ae5d9c0e2292e4edde98b
Felix Pouchain <flx.pouchain@gmail.com>
2024-04-11 11:31:29 +0200

    separate R setup in github actions

--- mobility/R/install_packages_from_cran.R
+++ mobility/R/install_packages_from_cran.R
@@ -9,21 +9,7 @@ if ("log4r" %in% rownames(installed.packages()) == FALSE) {
   )
 }
 
-if ("stringr" %in% rownames(installed.packages()) == FALSE) {
-  install.packages(
-    "stringi",
-    repos =  "https://packagemanager.rstudio.com/cran/latest",
-    quiet = TRUE,
-    configure.args="--disable-pkg-config"
-  )
-  install.packages(
-    "stringr",
-     repos =  "https://packagemanager.rstudio.com/cran/latest",
-    quiet = TRUE
-  )
-}
-
-packages <- packages[!(packages %in% c("log4r", "stringi", "stringr"))]
+packages <- packages[!(packages %in% c("log4r"))]
 
 library(log4r)
 logger <- logger(appenders = console_appender())
@@ -35,8 +21,6 @@ if (any(installed_packages == FALSE)) {
   install.packages(
     packages[!installed_packages],
     repos =  "https://packagemanager.rstudio.com/cran/latest",
-    quiet = FALSE
+    quiet = TRUE
   )
 }
-
-print(warnings())
