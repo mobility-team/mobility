0a7e455f7678d3ccbb1bbf0dc7fe7386e9bb9990
Félix Pouchain <flx.pouchain@gmail.com>
2024-05-02 15:40:26 +0200

    Revert "move tests"
    This reverts commit 0b8b7f60f0c600ffcfa99669c83aa7db50c405d2.

---
+++ tests_bkp/test_004_public_transport_costs_can_be_computed.py
@@ -0,0 +1,19 @@
+import mobility
+import pytest
+
+@pytest.mark.dependency(
+    depends=["tests/test_002_population_sample_can_be_created.py::test_002_population_sample_can_be_created"],
+    scope="session"
+ )
+def test_004_public_transport_costs_can_be_computed(test_data):
+    
+    transport_zones = mobility.TransportZones(
+        insee_city_id=test_data["transport_zones_insee_city_id"],
+        method="radius",
+        radius=test_data["transport_zones_radius"],
+    )
+    
+    pub_trans_travel_costs = mobility.PublicTransportTravelCosts(transport_zones)
+    pub_trans_travel_costs = pub_trans_travel_costs.get()
+    
+    assert pub_trans_travel_costs.shape[0] > 0
