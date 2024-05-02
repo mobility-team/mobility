2010e872b3fe4daf507f84c0209c35f31609c44f
Félix Pouchain <flx.pouchain@gmail.com>
2024-05-02 15:40:26 +0200

    Revert "Update test_transport_zones.py"
    This reverts commit 309ae86acae0de02bf877080444318f172b4926a.

--- tests/test_transport_zones.py
+++ tests/test_transport_zones.py
@@ -2,8 +2,6 @@ import os
 import dotenv
 import mobility
 
-mobility.set_params()
-
 def test_pub_trans():
   dotenv.load_dotenv()
   transport_zones = mobility.TransportZones("31404", method="radius", radius=40)
