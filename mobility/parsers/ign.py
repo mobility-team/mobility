82c64f1d38ddba3b5695ef0c13e24be38ec05a99
Félix Pouchain <flx.pouchain@gmail.com>
2024-05-02 15:40:26 +0200

    Revert "add a function to download osm regional extracts from geofabrik, crop them to match the transport zones boundary, extract only relevant ways for a given mode, merge the cropped and filtered files"
    This reverts commit 08226e1c474cc6c83cff445820eb4474937b390c.

--- mobility/parsers/ign.py
+++ mobility/parsers/ign.py
@@ -1,13 +1,14 @@
 import os
 import pathlib
 import requests
-import py7zr
+import zipfile
 import geopandas as gpd
 
 def prepare_ign(proxies={}):
     
+    # The directory path where dataFrames/ Parquets are stored
     data_folder_path = (
-        pathlib.Path.home() / ".mobility/data/ign/admin-express"
+        pathlib.Path(os.path.dirname(__file__)).parents[0] / "data/ign/admin-express"
     )
 
     if data_folder_path.exists() is False:
@@ -28,16 +29,16 @@ def prepare_ign(proxies={}):
         with open(path, "wb") as file:
             file.write(r.content)
 
-        # Unzip the content    
-        with py7zr.SevenZipFile(path, "r") as z:
-            z.extractall(data_folder_path)
+        # Unzip the content
+        with zipfile.ZipFile(path, "r") as zip_ref:
+            zip_ref.extractall(data_folder_path)
             
             
     # Convert to geoparquet
     path = data_folder_path / "ADMIN-EXPRESS-COG-CARTO_3-2__SHP_LAMB93_FXX_2023-05-03" / \
      "ADMIN-EXPRESS-COG-CARTO" / "1_DONNEES_LIVRAISON_2023-05-03" / "ADECOGC_3-2_SHP_LAMB93_FXX"
     
-    for shp_file in ["COMMUNE.shp", "EPCI.shp", "REGION.SHP"]:
+    for shp_file in ["COMMUNE.shp", "EPCI.shp"]:
             
          df = gpd.read_file(path / shp_file)
          parquet_file = pathlib.Path(shp_file).stem + ".parquet"
--- mobility/transport_zones.py
+++ mobility/transport_zones.py
@@ -7,10 +7,9 @@ import numpy as np
 
 from mobility.parsers.ign import prepare_ign
 from mobility.parsers.urban_units import prepare_urban_units
-from mobility.parsers.get_osm import get_osm
 
     
-def get_transport_zones(insee_city_id, method="epci_rings", radius=40):
+def get_transport_zones(insee_city_id, method="epci_rings", radius=None):
     """
     Returns a geodataframe of transport zones around a city of interest.
 
@@ -19,12 +18,10 @@ def get_transport_zones(insee_city_id, method="epci_rings", radius=40):
         method (str): method used to select cities around the city of interest.
             Can be one of the two methods :
             - epci_rings : all cities of the EPCIs that  are adjacent to the EPCI
-            of the city (first hop) or adjacent to the neighbor EPCIs (second hop).
+            of the city (first hop) or adjacent to the neighbor EPCIs (second hop)).
             - radius (all cities within a X km 
             radius around the centroid of the city)
-        n_hops (int): number 
-        radius (float): radius in km around the city to select cities, for the 
-            "radius" selection method.
+        radius (float): radius in km around the city to use for the "radius" selection method.
         
     Returns:
         geopandas.geodataframe: a geopandas geodataframe containing the following columns :
@@ -145,8 +142,3 @@ def __rgb_set_fixed__(self, instance, value):
             __old_rgb_set__(self, instance, WHITE)
 RGB.__set__ = __rgb_set_fixed__
 
-
-# Small test
-z = get_transport_zones("75056", method="radius", radius=40)
-transport_zones_boundary = z.to_crs(4326).unary_union
-osm_file_path = get_osm(transport_zones_boundary, verify="C:/Users/pouchaif/Documents/dev/forcepoint.pem")
