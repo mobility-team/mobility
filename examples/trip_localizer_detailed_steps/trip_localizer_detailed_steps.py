33fbd6ffc68c2dd05bbfba6b23c370aa0eee3970
Félix Pouchain <flx.pouchain@gmail.com>
2024-05-02 15:40:26 +0200

    Revert "change csv path to be case sensitive"
    This reverts commit a91d48e2963b4f6d34c99c024efbc682d638183d.

--- examples/trip_localizer_detailed_steps/trip_localizer_detailed_steps.py
+++ examples/trip_localizer_detailed_steps/trip_localizer_detailed_steps.py
@@ -16,7 +16,7 @@ travel_costs = mobility.MultimodalTravelCosts(transport_zones)
 trans_mode_cm = mobility.TransportModeChoiceModel(travel_costs, cost_of_time=20.0)
 work_dest_cm = mobility.WorkDestinationChoiceModel(transport_zones, travel_costs, cost_of_time=20.0, radiation_model_alpha=0.2, radiation_model_beta=0.8)
 
-population = mobility.Population(transport_zones, 100)
+population = mobility.Population(transport_zones, 1000)
 
 trips = mobility.Trips(population)
 loc_trips = mobility.LocalizedTrips(trips, cost_of_time=20.0, work_alpha=0.2, work_beta=0.8)
