6ec79a793fdeeff989a8098bbee921f7d80c9724
Félix Pouchain <flx.pouchain@gmail.com>
2024-05-02 15:40:26 +0200

    Revert "add openpyxl"
    This reverts commit e2d6e37bff882a92e0b1afb7b1dc7217b4d00dc8.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
     name="mobility-tools",
-    version="0.1",
-    author="Louise Gontier, Félix Pouchain, Capucine-Marin Dubroca-Voisin",
-    author_email="l.gontier@elioth.fr, felix.pouchain@arep.fr, capucine-marin.dubroca-voisin@arep.fr",
+    version="0.0.1",
+    author="Antoine Gauchot, Anne-Sophie Girot, Louise Gontier, Félix Pouchain",
+    author_email="antoine.gauchot@arep.fr, a.girot@elioth.fr, l.gontier@elioth.fr, felix.pouchain@arep.fr",
     description="A tool to simulate the mobility behaviours of the inhabitants of a given region.",
     url="https://github.com/mobility-team/mobility",
     packages=setuptools.find_packages(),
     python_requires=">=3.9",
-    install_requires=["numpy", "pandas", "requests", "pyarrow", "openpyxl"],
+    install_requires=["numpy", "pandas", "requests", "pyarrow"],
 )
