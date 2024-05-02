81d80498b6ca07ac988da0b5bf7c904e85ed7caf
Félix Pouchain <flx.pouchain@gmail.com>
2024-05-02 15:40:26 +0200

    Revert "Update test_transport_zones.py"
    This reverts commit dc629bfe3d1ed2477d2f32e8d03d0e55d85abcaa.

--- tests/test_transport_zones.py
+++ tests/test_transport_zones.py
@@ -4,7 +4,7 @@ import mobility
 import pathlib
 
 def test_pub_trans():
-  mobility.set_params(str(pathlib.Path.home() / ".mobility/data"),str(pathlib.Path.home() / ".mobility/data/projects"))
+  mobility.set_params(pathlib.Path.home() / ".mobility/data",pathlib.Path.home() / ".mobility/data/projects")
   dotenv.load_dotenv()
   transport_zones = mobility.TransportZones("31404", method="radius", radius=40)
   pub_trans_travel_costs = mobility.PublicTransportTravelCosts(transport_zones)
