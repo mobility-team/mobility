f3d6f3810edb941a81055dc5c71510c1f774658e
Felix Pouchain <flx.pouchain@gmail.com>
2024-05-13 18:19:44 +0200

    Initialization of the LocalAdminUnits class

---
+++ examples/switzerland/switzerland.py
@@ -0,0 +1,16 @@
+import os
+import dotenv
+import mobility
+import pandas as pd
+
+from mobility.parsers import LocalAdminUnits
+
+dotenv.load_dotenv()
+
+mobility.set_params(
+    package_data_folder_path=os.environ["MOBILITY_PACKAGE_DATA_FOLDER"],
+    project_data_folder_path=os.environ["MOBILITY_PROJECT_DATA_FOLDER"]
+)
+
+lau = LocalAdminUnits()
+lau.get()
