ae976371b023346beb603c703b22d2ea05f330f9
Joanna <gosse.joanna@gmail.com>
2024-05-15 18:38:07 +0200

    Add dep param

--- examples/Millau/pa.py
+++ examples/Millau/pa.py
@@ -6,6 +6,9 @@ from mobility.radiation_departments import *
 from mobility.school_map import *
 
 Age=2 # 1 pour les écoles marternelles et primaires, 2 pour les collèges et 3 pour les lycées
+dep = ["59"]
+model="radiation" #"radiation" pour utiliser le modèle de radiation, "proximity" pour utiliser le modèle de proximité, "school_map" pour utiliser le modèle de carte scolaire
+
 
 (
     sources_territory,
@@ -13,10 +16,10 @@ Age=2 # 1 pour les écoles marternelles et primaires, 2 pour les collèges et 3 
     costs_territory,
     coordinates,
     raw_flowDT,
-) = get_data_for_model_school(["59"], Age)
+) = get_data_for_model_school(dep, Age)
 
 
-model="radiation" #"radiation" pour utiliser le modèle de radiation, "proximity" pour utiliser le modèle de proximité, "school_map" pour utiliser le modèle de carte scolaire
 flowsRM, flowDT, coordinates_res, plot_sources=run_model_for_territory(sources_territory, sinks_territory, costs_territory, coordinates, raw_flowDT,model,"school")
 ssi2=compute_similarity_index(flowsRM, flowDT)
 print(ssi2)
+
