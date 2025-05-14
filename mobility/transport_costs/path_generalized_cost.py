fa5aa62ecc60d851085ce16fc0bb2fea35547002
Felix Pouchain <flx.pouchain@gmail.com>
2025-05-14 09:17:54 +0200

    compute total distance for modes with multiple submodes

--- mobility/transport_costs/path_generalized_cost.py
+++ mobility/transport_costs/path_generalized_cost.py
@@ -13,6 +13,8 @@ class PathGeneralizedCost(InMemoryAsset):
         
     def get(self, metrics=["cost"], congestion: bool = False) -> pd.DataFrame:
         
+        print("get distance")
+        
         costs = self.travel_costs.get(congestion)
         
         study_area = self.travel_costs.transport_zones.study_area.get()
