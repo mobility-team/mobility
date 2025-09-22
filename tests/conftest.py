c71158b45b5771d80965d4986288a9fe0ca67151
Cap <capucine-marin.dubroca-voisin@arep.fr>
2025-09-22 16:47:16 +0200

    test with Foix

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ def pytest_addoption(parser):
     parser.addoption("--local", action="store_true", default=False)
     parser.addoption("--clear_inputs", action="store_true", default=False)
     parser.addoption("--clear_results", action="store_true", default=False)
-    
+
 @pytest.fixture(scope="session")
 def clear_inputs(request):
     return request.config.getoption("--clear_inputs")
@@ -23,24 +23,24 @@ def local(request):
     return request.config.getoption("--local")
 
 def do_mobility_setup(local, clear_inputs, clear_results):
-    
+
     if local:
-        
+
         dotenv.load_dotenv()
-        
+
         if clear_inputs is True:
             shutil.rmtree(os.environ["MOBILITY_PACKAGE_DATA_FOLDER"], ignore_errors=True)
-        
+
         if clear_results is True:
             shutil.rmtree(os.environ["MOBILITY_PACKAGE_PROJECT_FOLDER"], ignore_errors=True)
-        
+
         mobility.set_params(
             package_data_folder_path=os.environ["MOBILITY_PACKAGE_DATA_FOLDER"],
             project_data_folder_path=os.environ["MOBILITY_PACKAGE_PROJECT_FOLDER"]
         )
-        
+
     else:
-        
+
         mobility.set_params(
             package_data_folder_path=pathlib.Path.home() / ".mobility/data",
             project_data_folder_path=pathlib.Path.home() / ".mobility/projects/tests",
@@ -58,11 +58,11 @@ def setup_mobility(local, clear_inputs, clear_results):
 
 def get_test_data():
     return {
-        "transport_zones_local_admin_unit_id": "fr-12202",
+        "transport_zones_local_admin_unit_id": "fr-09122", #Foix instead of Rodez 12202
         "transport_zones_radius": 10.0,
         "population_sample_size": 10
     }
-        
+
 @pytest.fixture
 def test_data():
     return get_test_data()
