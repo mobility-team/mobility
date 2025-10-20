87ab642fb9732abc45a015f15064462afc942113
BENYEKKOU ADAM <adam.benyekkou@arep.fr>
2025-10-20 12:00:29 +0200

    Moved test_004 now that conflict is solved, checking

--- tests/back/integration/test_004_public_transport_costs_can_be_computed.py
+++ tests/back/integration/test_004_public_transport_costs_can_be_computed.py
@@ -1,11 +1,20 @@
 import mobility
 import pytest
 
+# Uncomment the next lines if you want to test interactively, outside of pytest, 
+# but still need the setup phase and input data defined in conftest.py
+# Don't forget to recomment or the tests will not pass !
+
+# from conftest import get_test_data, do_mobility_setup
+# do_mobility_setup(True, False, False)
+# test_data = get_test_data()
+
 @pytest.mark.dependency(
     depends=["tests/test_002_population_sample_can_be_created.py::test_002_population_sample_can_be_created"],
     scope="session"
-)
+ )
 def test_004_public_transport_costs_can_be_computed(test_data):
+    
     transport_zones = mobility.TransportZones(
         local_admin_unit_id=test_data["transport_zones_local_admin_unit_id"],
         radius=test_data["transport_zones_radius"]
@@ -45,8 +54,6 @@ def test_004_public_transport_costs_can_be_computed(test_data):
 
     costs = public_transport.travel_costs.get()
     gen_costs = public_transport.generalized_cost.get(["distance", "time"])
-
+    
     assert costs.shape[0] > 0
     assert gen_costs.shape[0] > 0
-    assert gen_costs.shape[1] > 0
-    assert set(["distance", "time"]).issubset(gen_costs.columns)
