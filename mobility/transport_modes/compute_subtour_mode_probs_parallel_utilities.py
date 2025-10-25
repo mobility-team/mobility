3f245d833b7e6d37abde3ce67673ce1bc3309fd6
Félix Pouchain <flx.pouchain@gmail.com>
2025-10-25 23:30:56 +0200

    Correction d'une erreur dans le calcul des probabilités lorsque les coûts sont très élevés v2 (#215)
    * correct handling of costs / utilities / probs
    
    * fix conflict
    
    ---------
    
    Co-authored-by: Cap <capucine-marin.dubroca-voisin@arep.fr>

--- mobility/transport_modes/compute_subtour_mode_probs_parallel_utilities.py
+++ mobility/transport_modes/compute_subtour_mode_probs_parallel_utilities.py
@@ -281,11 +281,17 @@ def run_top_k_search(
     
     else:
         
-        c = np.array([r[0] for r in results])
-        c = c - np.max(c)
-        p = np.exp(-c)
-        p /= p.sum()
-        i_max = np.argmax(p.cumsum() > 0.98)
+        # Transform costs into utilities
+        # (remove the max so that exponentials don't overflow in the next step)
+        utilities = -np.array([r[0] for r in results])
+        utilities -= np.max(utilities)
+        
+        # Compute probabilities
+        prob = np.exp(utilities)
+        prob = prob/prob.sum()
+        
+        # Keep only the first 98 % of the cumulative distribution
+        i_max = np.argmax(prob.cumsum() > 0.98)
           
         rows = []
         for i, (total_cost, mode_seq) in enumerate(results):
